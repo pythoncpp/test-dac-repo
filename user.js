fa7422549ee1a843151bfdc723f4754fda260e80
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2023-07-05 09:09:06 +0530

    added code to authenticate

--- user.js
+++ user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/signin', (request, response) => {
+  // this is the code to authenticate the user
   response.send('done')
 })
 
